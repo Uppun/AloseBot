98f2c715f3a637e6ffdd73f58f876acf8e056ad1
Uppun <TheSuperBro@gmail.com>
2020-01-30 11:14:45 -0500

    assets

---
+++ assets/coins.js
@@ -0,0 +1,10 @@
+const Discord = require('discord.js');
+const fs = require('fs');
+const codes = fs.readdirSync('./assets/CODES');
+
+const codeMap = new Map();
+for (const code of codes) {
+    codeMap.set(code.substr(0, code.length-4), new Discord.Attachment(`./assets/CODES/${code}`, `${code}`));
+}
+
+module.exports = codeMap;
