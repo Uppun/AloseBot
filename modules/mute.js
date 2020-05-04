2f2e12a0f82c4642ecb333454b1cfccd2c4667ec
Uppun <TheSuperBro@gmail.com>
2020-05-04 08:23:15 -0400

    fixed mute it's live now

--- modules/mute.js
+++ modules/mute.js
@@ -12,26 +12,27 @@ class MuteModule {
             const muteId = this.config.get('mute-id');
             if (message.member.roles.find(r => modIds.includes(r.id))) {
                 const args = message.content.split(' ');
-                const userId = args[1];
+                const userId = args[1].match(/\d+/g);
                 const muteTime = args[2];
-                if (!userId || !muteTime) {
+                if (!userId[0] || !muteTime) {
                     return message.channel.send('The command is `?mute @user time`!');
                 }
-                const muted = message.members.guild.get(userId);
+                const muted = message.guild.members.get(userId[0]);
                 if (!muted) {
                     return message.channel.send('I couldn\'t find that user...');
                 }
-                if (!muted.member.roles.find(r => muteId === r.id)) {
+
+                if (!muted.roles.find(r => muteId === r.id)) {
                     muted.addRole(muteId).then(member => {
                         message.channel.send(`I\'ve muted that user for ${muteTime} minutes!`);
-                        member.send(`You have been muted in the Doghouse server for ${muteTime} minutes for bad behavior.`);
+                        member.send(`Hello, I'm here to inform you that you've been muted in the Doghouse server for ${muteTime} minutes. Take this time to reflect and relax, and we'll see you soon.`);
                     });
                 }
 
                 this.mutedUsers[muteId] = setTimeout(() => {
                     if (muted) {
                         muted.removeRole(muteId);
-                        muted.send('The mute has expired!');
+                        muted.send('Hello again, here to let you know that the mute has expired now. Welcome back! :purple_heart:');
                     }
                 }, parseInt(muteTime, 10) * 60000);
             }
@@ -42,24 +43,24 @@ class MuteModule {
             const muteId = this.config.get('mute-id');
             if (message.member.roles.find(r => modIds.includes(r.id))) {
                 const args = message.content.split(' ');
-                const userId = args[1];
-                if (!userId) {
+                const userId = args[1].match(/\d+/g);
+                if (!userId[0]) {
                     return message.channel.send('The command is `?unmute @user`!');
                 }
-                const unMuted = message.members.guild.get(userId);
+                const unMuted = message.guild.members.get(userId[0]);
                 if (!unMuted) {
                     return message.channel.send('I couldn\'t find that user...');
                 }
-                if (!unMuted.member.roles.find(r => muteId === r.id)) {
+                if (!unMuted.roles.find(r => muteId === r.id)) {
                     return message.channel.send('That user is not currently muted.');
                 }
 
                 unMuted.removeRole(muteId).then(member => {
                     message.channel.send(`I\'ve un-muted that user!`);
-                    member.send(`Your mute has been manually removed!`);
+                    member.send(`Hello again, here to let you know that the mute has been manually removed. Welcome back! :purple_heart:`);
                 });
 
-                if (this.mutedUser[muteId]) {
+                if (this.mutedUsers[muteId]) {
                     clearTimeout(this.mutedUsers[muteId]);
                     delete this.mutedUsers[muteId];
                 }
