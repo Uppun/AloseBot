b23b36307920e4b0c08697c0df45948d58f4c5fe
Uppun <TheSuperBro@gmail.com>
2020-08-16 14:29:11 -0400

    modifying keywords for longer lists

--- modules/keywords.js
+++ modules/keywords.js
@@ -88,13 +88,57 @@ class KeywordModule {
 
         this.dispatch.hook('!wordlist', (message) => {
             const channel = this.config.get('bot-channel');
-
             if (channel === message.channel.id) {
-                let string = 'My phrase associations are: \n'
-                for (const [key, value] of Object.entries(this.keyWords)) {
-                    string += `${key} => ${value}\n`;
+                const wordPairs = Object.entries(this.keyWords);
+                if (wordPairs.length < 1) {
+                    return message.channel.send('I don\'t have any phrase associations!');
                 }
-                message.channel.send(string);
+                const pages = [];
+                let page = ``;
+                let entriesNum = 0;
+                for (let i = 0; i < wordPairs.length; i++) {
+                    if (entriesNum === 10) {
+                        pages.push(page);
+                        page = ``;
+                        entriesNum = 0
+                    }
+                    entriesNum++;
+                    page += `${wordPairs[i][0]} => ${wordPairs[i][1]}`;
+                }
+                if (page !== ``) {
+                    pages.push(page);
+                }
+
+                let currentPage = 0;
+                const wordEmbed = new Discord.RichEmbed()
+                    .setAuthor(`Alose`)
+                    .setFoot(`Page ${currentPage+1} of ${pages.length}`)
+                    .setDescription(pages[currentPage]);
+                
+                message.channel.send(wordEmbed).then(msg => {
+                    const reactionMap = new Map([['⏪', -1],['⏩', 1]]);
+
+                    Array.from(reactionMap.keys()).reduce( async (previousPromise, nextReaction) => {
+                        await previousPromise;
+                        return msg.react(nextReaction)
+                    }, Promise.resolve());
+                    
+                    const reactionCollector = new Discord.ReactionCollector(msg, (reaction, user) => {
+                        return reactionMap.has(reaction.emoji.name) && user.id !== this.client.user.id;
+                    }, { time: 600000, });
+        
+                    reactionCollector.on('collect', (reaction, collector) => {
+                        let pageChange = currentPage + reactionMap.get(reaction.emoji.name);
+        
+                        if ((pageChange > -1) && (pageChange < pages.length)) {
+                            currentPage = pageChange;
+                            wordEmbed.setDescription(pages[currentPage]);
+                            wordEmbed.setFooter(`Page ${currentPage+1} of ${pages.length}`);
+                            msg.edit(wordEmbed);
+                        }
+                        removeReactionUsers(reaction, this.client.user.id);
+                    })
+                });
             }
         });
 
